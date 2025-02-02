6ec973ab73ef25d3ad1b58ea19d27af98d04f242
vermantjarne <vermant.jarne@gmail.com>
2025-02-02 17:26:29 +0100

    Finish assignment for Props And Custom Events

---
+++ assignments/06. Props And Custom Events/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<active-user
+			:username="user.name"
+			:userage="user.age"
+		></active-user>
+		<user-data
+			@update-data="updateData"
+		></user-data>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			user: {
+				name: 'Jarne Vermant',
+				age: 21,
+			}
+		}
+	},
+	methods: {
+		updateData(name, age) {
+			this.user = {
+				name: name,
+				age: +age,
+			}
+		}
+	}
+}
+</script>
+
+<style>
+	section {
+		margin: 2rem auto;
+		max-width: 40rem;
+		border-radius: 12px;
+		border: 1px solid #ccc;
+		padding: 10px;
+	}
+
+	label {
+		margin-right: 5px;
+	}
+</style>
---
+++ assignments/06. Props And Custom Events/src/components/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+	<section>
+		<form @submit.prevent="submitData">
+			<div>
+				<label>Name</label>
+				<input type="text" v-model="enteredName">
+			</div>
+			<div>
+				<label>Age</label>
+				<input type="text" v-model="enteredAge">
+			</div>
+			<button>Update Data</button>
+		</form>
+	</section>
+</template>
+
+<script>
+export default {
+	emits: [
+		'update-data'
+	],
+	data() {
+		return {
+			enteredName: '',
+			enteredAge: '',
+		}
+	},
+	methods: {
+		submitData() {
+			this.$emit('update-data', this.enteredName, this.enteredAge);
+		}
+	}
+}
+</script>
